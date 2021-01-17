2ae7cc377fdb90b19b1af0bb12c711abfd3aea0d
beratakuzum <beratakuzum34@gmail.com>
2021-01-17 23:16:06 +0300

    changed single quotes to double quotes

--- event_schemas.py
+++ event_schemas.py
@@ -1,15 +1,32 @@
-tempsensor = {
-    '$schema': 'http://json-schema.org/schema#',
-    'type': 'object',
-    'properties': {
-        'temperature': {
+"""Json schemas to validate events"""
+
+temp_sensor = {
+    "$schema": "http://json-schema.org/schema#",
+    "type": "object",
+    "properties": {
+        "temperature": {
             "type": "number"
         },
-        'timestamp': {
+        "timestamp": {
             "type": "string",
             "format": "date-time"
         }
     },
     "additionalProperties": False,
-    'required': ['temperature', 'timestamp']
+    "required": ["temperature", "timestamp"]
+}
+pressure_sensor = {
+    "$schema": "http://json-schema.org/schema#",
+    "type": "object",
+    "properties": {
+        "pressure": {
+            "type": "number"
+        },
+        "timestamp": {
+            "type": "string",
+            "format": "date-time"
+        }
+    },
+    "additionalProperties": False,
+    "required": ["temperature", "timestamp"]
 }
