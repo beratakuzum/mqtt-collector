6e67a27d99aa5e463ca55b64166d2a931d445eea
beratakuzum <beratakuzum34@gmail.com>
2021-01-17 23:55:55 +0300

    small refactor

--- examples/publish.py
+++ examples/publish.py
@@ -2,20 +2,22 @@
 An example mqtt publisher to test the pipeline.
 Just run the file as 'python publish.py'
 """
-
 import json
+import uuid
 
-import paho.mqtt.client as mqtt  # import the client1
-from bson import ObjectId
+import paho.mqtt.client as mqtt
 
 topic = "events/temp_sensor"
 
 broker_host = 'localhost'
-client = mqtt.Client(str(ObjectId))  # create new instance
-client.connect(broker_host, 1883)  # connect to broker
+client = mqtt.Client(str(uuid.uuid4()))  # create new client with a unique id
+client.connect(broker_host, 1883)  # connect the to broker
 
 data = {
     "temperature": 1,
     "timestamp": "2018-11-13T20:20:39+00:00"
 }
-client.publish("events/temp_sensor", json.dumps(data), qos=0)
+
+# serializing json to str
+formatted_data = json.dumps(data)
+client.publish(topic, formatted_data, qos=0)
